545b7edf3b1987a9ae786bb4fb0daf8566e7c7c8
jshtaylor <jshtaylor11@gmail.com>
2026-09-21 12:17:42 -0400

    added NasaLaunch, fixed some minor changes in the header file

---
+++ NasaLaunchAnalysis.cpp
@@ -0,0 +1,27 @@
+#include "TimeCode.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+TimeCode parse_line(string){
+//implement this to take a lone from teh file and return the time code from said line
+
+}
+
+vector<string> split(string, char delim){
+//uses delim to split the line into each individual word, probably based on either space or comma
+
+
+}
+
+
+
+
+
+int main(){
+//should return an average of 12:7:56 based on the math numbers
+return 0;
+}
